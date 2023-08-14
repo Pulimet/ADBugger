521c8e47459575b73b388270c0780cba2a0ee610
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2023-08-14 12:45:41 +0300

    Window size fixes

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,12 @@
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
@@ -13,6 +15,7 @@ import androidx.compose.ui.window.rememberWindowState
 import store.AppStore
 import ui.MainContent
 import ui.theme.MyColors
+import java.awt.Color
 
 fun main() = application {
     val state = rememberWindowState(
@@ -23,6 +26,7 @@ fun main() = application {
 
     val model = remember { AppStore() }
     val coroutineScope = rememberCoroutineScope()
+    val density = LocalDensity.current
 
     ApplicationTray(state)
 
@@ -32,8 +36,13 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         onKeyEvent = { model.onKeyEvent(coroutineScope, it) }
     ) {
-        Surface(color = MyColors.bg, modifier = Modifier.wrapContentSize().onSizeChanged {
-            window.setSize(window.width, it.height + 32)
+        LaunchedEffect(Unit) {
+            window.background = Color(0x252734)
+        }
+        Surface(color = MyColors.bg, modifier = Modifier.composed {
+            Modifier.onSizeChanged {
+                window.setSize(window.width, (it.height / density.density).toInt() + 32)
+            }
         }) {
             MainContent(model, coroutineScope)
         }
