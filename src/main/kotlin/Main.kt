251e49a0c4b3ed6480b5c42ca5952a2832cf8cd7
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2024-03-25 09:17:55 +0200

    Window - Disable auto height size change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
@@ -19,14 +15,13 @@ import java.awt.Color
 
 fun main() = application {
     val state = rememberWindowState(
-        width = 500.dp,
-        height = 50.dp,
-        position = WindowPosition(alignment = Alignment.TopEnd)
+        width = 1000.dp,
+        height = 600.dp,
+        position = WindowPosition(alignment = Alignment.TopCenter)
     )
 
     val model = remember { AppStore() }
     val coroutineScope = rememberCoroutineScope()
-    val density = LocalDensity.current
 
     ApplicationTray(state)
 
@@ -39,11 +34,7 @@ fun main() = application {
         LaunchedEffect(Unit) {
             window.background = Color(0x252734)
         }
-        Surface(color = MyColors.bg, modifier = Modifier.composed {
-            Modifier.onSizeChanged {
-                window.setSize(window.width, (it.height / density.density).toInt() + 32)
-            }
-        }) {
+        Surface(color = MyColors.bg) {
             MainContent(model, coroutineScope)
         }
     }
