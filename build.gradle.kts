08336f9cd289d9ade7393af3acf347ce890fb7f3
Pulimet <korolev2009@gmail.com>
2024-09-19 13:33:35 +0300

    Update br.com.devsrsouza.compose.icons 1.1.0 -> 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
         "erikflowers-weather-icons",
         "css-gg"
     ).forEach {
-        implementation("br.com.devsrsouza.compose.icons:$it:1.1.0")
+        implementation("br.com.devsrsouza.compose.icons:$it:1.1.1")
     }
 }
 
