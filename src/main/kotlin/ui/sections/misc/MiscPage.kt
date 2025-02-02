885608e16a5069a1663d051d6a477358a37f8716
Pulimet <korolev2009@gmail.com>
2025-02-02 09:09:16 +0200

    MiscTabA.kt - Remove hard coded strings

--- src/main/kotlin/ui/sections/misc/MiscPage.kt
+++ src/main/kotlin/ui/sections/misc/MiscPage.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import net.alexandroid.adbugger.adbugger.generated.resources.Res
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_tab_1
+import org.jetbrains.compose.resources.stringResource
 import ui.theme.Dimensions
 import ui.widgets.CardX
 import ui.widgets.tabs.Tabs
@@ -12,7 +15,7 @@ import ui.widgets.tabs.Tabs
 fun MiscPage(modifier: Modifier = Modifier) {
     CardX(modifier = modifier) {
         Column(modifier = Modifier.padding(Dimensions.cardPadding)) {
-            Tabs(listOf("Tab 1"), showTabs = false) {
+            Tabs(listOf(stringResource(Res.string.misc_tab_1)), showTabs = false) {
                 when (it) {
                     0 -> MiscTabA()
                 }
--- src/main/kotlin/ui/sections/misc/MiscTabA.kt
+++ src/main/kotlin/ui/sections/misc/MiscTabA.kt
@@ -15,6 +15,20 @@ import androidx.compose.ui.unit.dp
 import compose.icons.LineaIcons
 import compose.icons.lineaicons.Basic
 import compose.icons.lineaicons.basic.Home
+import net.alexandroid.adbugger.adbugger.generated.resources.Res
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_airplane
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_auto_off
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_auto_on
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_land_2
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_landscape
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_location_mode
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_off
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_on
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_portrait
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_portrait_2
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_rotation
+import net.alexandroid.adbugger.adbugger.generated.resources.misc_wifi_mode
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import store.AppStore
 import ui.theme.Dimensions
@@ -23,76 +37,76 @@ import ui.widgets.buttons.BtnWithText
 @Composable
 fun MiscTabA(model: AppStore = koinInject()) {
     Column(modifier = Modifier.fillMaxSize()) {
-        SectionTitle("Airplane mode")
+        SectionTitle(stringResource(Res.string.misc_airplane))
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onAirplaneOn() },
-                description = "On",
+                description = stringResource(Res.string.misc_on),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onAirplaneOff() },
-                description = "Off",
+                description = stringResource(Res.string.misc_off),
             )
         }
-        SectionTitle("Wifi mode")
+        SectionTitle(stringResource(Res.string.misc_wifi_mode))
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onWifiOn() },
-                description = "On",
+                description = stringResource(Res.string.misc_on),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onWifiOff() },
-                description = "Off",
+                description = stringResource(Res.string.misc_off),
             )
         }
-        SectionTitle("Rotation")
+        SectionTitle(stringResource(Res.string.misc_rotation))
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationAutoTurnOn() },
-                description = "Auto On",
+                description = stringResource(Res.string.misc_auto_on),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationAutoTurnOff() },
-                description = "Auto Off",
+                description = stringResource(Res.string.misc_auto_off),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationLandscape() },
-                description = "Landscape",
+                description = stringResource(Res.string.misc_landscape),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationPortrait() },
-                description = "Portrait",
+                description = stringResource(Res.string.misc_portrait),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationPortraitUpSideDown() },
-                description = "Portrait 2",
+                description = stringResource(Res.string.misc_portrait_2),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onRotationLandscapeUpSideDow() },
-                description = "Land. 2",
+                description = stringResource(Res.string.misc_land_2),
             )
         }
-        SectionTitle("Location mode")
+        SectionTitle(stringResource(Res.string.misc_location_mode))
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onGpsOn() },
-                description = "On",
+                description = stringResource(Res.string.misc_on),
             )
             BtnWithText(
                 icon = LineaIcons.Basic.Home,
                 onClick = { model.onGpsOff() },
-                description = "Off",
+                description = stringResource(Res.string.misc_off),
             )
         }
     }
